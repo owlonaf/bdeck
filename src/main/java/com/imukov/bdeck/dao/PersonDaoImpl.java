9c3ad5876fe3fa940d7d125816a5d0b0e7fa875c
imukov <imukov@o-code.ru>
2021-09-17 11:41:57 +0400

    entitymanager was added

--- src/main/java/com/imukov/bdeck/dao/PersonDaoImpl.java
+++ src/main/java/com/imukov/bdeck/dao/PersonDaoImpl.java
@@ -3,22 +3,33 @@ package com.imukov.bdeck.dao;
 import com.imukov.bdeck.domain.PersonEntity;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.UUID;
 
 @Repository
 public class PersonDaoImpl implements PersonDao{
 
-    
+    @PersistenceContext
+    private EntityManager entityManager;
 
     @Override
     public void createUser(String name, String nickname, String password) {
-        sessionFactory.getCurrentSession().save(new PersonEntity(UUID.randomUUID(), name, nickname, password));
+        entityManager.getTransaction().begin();
+        entityManager.persist(new PersonEntity(UUID.randomUUID(), name, nickname, password));
+        entityManager.getTransaction().commit();
     }
 
     @Override
     public void deleteUser(String nickname, String password) {
-        sessionFactory.getCurrentSession().delete(PersonEntity.class.);
+    entityManager.getTransaction().begin();
+    PersonEntity person = entityManager.find(PersonEntity.class, new String(nickname));
+    if (person.getPassword() == password){
+        entityManager.remove(person);
+    }
+    entityManager.getTransaction().commit();
     }
 }
