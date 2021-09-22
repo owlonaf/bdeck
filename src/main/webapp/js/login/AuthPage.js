52368f5bb6de2adceaf0111806744a230547ec1b
imukov <imukov@o-code.ru>
2021-09-22 17:19:04 +0400

    front...

---
+++ src/main/webapp/js/login/AuthPage.js
@@ -0,0 +1,79 @@
+Ext.define('bdeck.login.AuthPage', {
+    extend: 'Ext.panel.Panel',
+    width: 500,
+    height: 360,
+    alias: 'widget.authPage',
+
+    initComponent : function () {
+        var me = this;
+
+        var nameField = Ext.widget({
+            xtype: 'textfield',
+            name: 'name',
+            fieldLabel: 'Name',
+            margin: '10 0 0 5',
+            allowBlank: false
+        });
+
+        var nicknameField = Ext.widget({
+            xtype: 'textfield',
+            name: 'nickname',
+            fieldLabel: 'Nickname',
+            margin: '10 0 0 5',
+            allowBlank: false
+        });
+
+        var passwordField = Ext.widget({
+            xtype: 'textfield',
+            name: 'password',
+            fieldLabel: 'Password',
+            margin: '10 0 0 5',
+            allowBlank: false
+        });
+
+        var confirmPasswordField = Ext.widget({
+            xtype: 'textfield',
+            name: 'confirmPassword',
+            fieldLabel: 'Confirm Password',
+            margin: '10 0 0 5',
+            allowBlank: false
+        });
+
+        var textt = {
+            xtype: 'text',
+            itemId: 'textt',
+            text: 'jfjdjd'
+        };
+
+        var confirmButton = Ext.create('Ext.Button', {
+            text: 'Click me',
+            handler: function() {
+                var name = nameField.getValue();
+                var nickname = nicknameField.getValue();
+                var password = passwordField.getValue();
+                var confirmPassword = confirmPasswordField.getValue();
+            }
+        });
+
+        Ext.apply(me, {
+            items: Ext.create('Ext.Panel', {
+                title: "HBoxLayout Panel",
+                layout: 'hbox',
+                items: [
+                    Ext.create('Ext.Panel', {
+                        title: "VBoxLayout Panel",
+                        layout:'vbox',
+                        items: [
+                            nameField,
+                            nicknameField,
+                            passwordField,
+                            confirmPasswordField,
+                        ]
+                    }),
+                    confirmButton,
+                    textt,
+                ]
+            })
+        })
+    }
+});
