40c0341facc92d4002f8b9dbd3de52488fad8e8a
owlonaf <imviktor0170@gmail.com>
2021-10-18 20:45:39 +0400

    kruto!

--- src/main/java/com/imukov/bdeck/controller/AuthorizationController.java
+++ src/main/java/com/imukov/bdeck/controller/AuthorizationController.java
@@ -3,22 +3,15 @@ package com.imukov.bdeck.controller;
 import com.imukov.bdeck.service.LogService;
 import com.imukov.bdeck.vo.PersonVo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+@Controller
 public class AuthorizationController {
 
     @Autowired
     private LogService logService;
-
-//    @RequestMapping(method = RequestMethod.POST)
-//    public PersonVo registration (@RequestParam("name") String name,
-//                                  @RequestParam("nickname") String nickname,
-//                                  @RequestParam("password") String password,
-//                                  @RequestParam("confirmPassword") String confirmPassword){
-//        return logService.registred(name, nickname, password, confirmPassword);
-//    }
 
     @RequestMapping(value = { "/"}, method = RequestMethod.GET)
     public String index(Model model) {
