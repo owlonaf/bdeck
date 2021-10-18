63cfafb5cbf62c43a5faea143f4e0b8f1054b029
imukov <imukov@o-code.ru>
2021-10-18 13:24:56 +0400

    nihuya ne poluchaetsya

--- src/main/java/com/imukov/bdeck/BdeckApplication.java
+++ src/main/java/com/imukov/bdeck/BdeckApplication.java
@@ -1,6 +1,5 @@
 package com.imukov.bdeck;
 
-import com.imukov.bdeck.domain.PersonEntity;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,5 +9,4 @@ public class BdeckApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BdeckApplication.class, args);
 	}
-    PersonEntity personEntity = new PersonEntity("name", "nickname", "password");
 }
--- src/main/java/com/imukov/bdeck/controller/AuthorizationController.java
+++ src/main/java/com/imukov/bdeck/controller/AuthorizationController.java
@@ -3,20 +3,27 @@ package com.imukov.bdeck.controller;
 import com.imukov.bdeck.service.LogService;
 import com.imukov.bdeck.vo.PersonVo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/auth")
 public class AuthorizationController {
 
     @Autowired
     private LogService logService;
 
-    @RequestMapping(method = RequestMethod.POST)
-    public PersonVo registration (@RequestParam("name") String name,
-                                  @RequestParam("nickname") String nickname,
-                                  @RequestParam("password") String password,
-                                  @RequestParam("confirmPassword") String confirmPassword){
-        return logService.registred(name, nickname, password, confirmPassword);
+//    @RequestMapping(method = RequestMethod.POST)
+//    public PersonVo registration (@RequestParam("name") String name,
+//                                  @RequestParam("nickname") String nickname,
+//                                  @RequestParam("password") String password,
+//                                  @RequestParam("confirmPassword") String confirmPassword){
+//        return logService.registred(name, nickname, password, confirmPassword);
+//    }
+
+    @RequestMapping(value = { "/"}, method = RequestMethod.GET)
+    public String index(Model model) {
+        String message = "Hello Spring Boot + JSP";
+        model.addAttribute("message", message);
+        return "index";
     }
 }
