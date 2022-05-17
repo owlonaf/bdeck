a12f83cf16b779e532e3feb924988a67fc1fb9fc
owlonaf <imviktor0170@gmail.com>
2022-05-18 02:09:06 +0400

    new login's config

--- src/main/java/com/imukov/bdeck/configuration/WebSecurityConfig.java
+++ src/main/java/com/imukov/bdeck/configuration/WebSecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -57,14 +58,28 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 //                .authorizeRequests().anyRequest().permitAll()
                 .authorizeRequests().antMatchers("/authenticate", "/register").permitAll().
                 // all other requests need to be authenticated
-                        anyRequest().authenticated().and().
-                // make sure we use stateless session; session won't be used to
-                // store user's state.
-                        exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and().sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                        anyRequest().authenticated().and()
+                .formLogin()
+                .loginPage("/landing.html")
+                .failureUrl("/landing.html?error=true").permitAll()
+        // make sure we use stateless session; session won't be used to
+        // store user's state.
+//                .exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and().sessionManagement()
+//                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         ;
 
         // Add a filter to validate the tokens with every request
         httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        super.configure(web);
+
+        web
+                .ignoring()
+                .antMatchers("/resources/**", "/css/**", "/js/**", "/images/**", "/fonts/**");
+
+
     }
 }
